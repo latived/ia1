65a8114f2b9ff20262f258963584598a530ae2ed
vitalwarley <wvb@ic.ufal.br>
2018-04-15 10:54:44 -0300

    dfs and bfs seems to be working...

--- eight_puzzle.py
+++ eight_puzzle.py
@@ -1,99 +1,122 @@
 #!/usr/bin/env python3
 
-# state space is the range of configurations we can reach when we apply
-# procedures, that is, we move the blank space 
-# (NOTE: moves are four = up, right, down, left;
-# but procedures are limited by parameters/actual configuration)
-#
-# state space for this puzzle is a graph
-#
-# state can be a simple 3x3 array
-#
-# beginning state: anything?
-#   1 4 3       _ 8 1
-#   7 _ 6   or  2 7 3 or ... 
-#   5 8 2       4 6 5
-# 
-# goal state: 
-#   1 2 3
-#   4 5 6
-#   7 8 _
-#
-# to list both beginning and goal states have the form:
-#   bs: 1 4 3 7 0 6 5 8 2 or 0 8 1 2 7 3 4 6 5 or ...
-#   gs: 1 2 3 4 5 6 7 8 0
-#
-# in this case, a valid move would be, from a certain pos (1-9):
-# 1 -> 2,4
-# 2 -> 1,3,5
-# 3 -> 2,6
-# 4 -> 1,5,7
-# 5 -> 2,4,6,8
-# 6 -> 3,5,9
-# 7 -> 4,8
-# 8 -> 5,7,9
-# 9 -> 6,8
-#
-# parameters: 9, location of numbers in the grid
-
-valid_moves = {1 : [2,4],
-               2 : [1,3,5],
-               3 : [2,6],
-               4 : [1,5,7],
-               5 : [2,4,6,8],
-               6 : [3,5,9],
-               7 : [4,8],
-               8 : [5,7,9],
-               9 : [6,8]}
+valid_moves = {0 : [1,3],
+               1 : [0,2,4],
+               2 : [1,5],
+               3 : [0,4,6],
+               4 : [1,3,5,7],
+               5 : [2,8,4],
+               6 : [3,7],
+               7 : [4,8,6],
+               8 : [5,7]}
 
 def check_state(state):
-    for i in range(8):
-        if i+1 != state[i]:
-            return False
-    if state[8] != 0: return False
-    return True
+    #goal_state = [1, 2, 3, 4, 5, 6, 7, 8, 0]
+    goal_state = [1, 2, 3, 8, 0, 4, 7, 6, 5]
+    return state == goal_state
 
-def show_state(state):
-    print("{0} {1} {2}\n{3} {4} {5}\n{6} {7} {8}\n".format(
-            state[0], state[1], state[2],
-            state[3], state[4], state[5],
-            state[6], state[7], state[8])
-            )
+def show_states_conf(state):
+    print("\t{0} {1} {2}\n\t{3} {4} {5}\n\t{6} {7} {8}\n".format(
+        state[0], state[1], state[2],
+        state[3], state[4], state[5],
+        state[6], state[7], state[8]
+        )
+        )
 
-def iterativo(state):
+def iterative(state):
     print("Não implementado.")
 
-# na solução poderá aparecer mais de um caminho válido
-moves = []
-found = False
-def dfs(state):
-    if check_state(state):
-        print("-> solucionado! total de estados procurados: {}".format(len(moves)))
-        global found 
-        found = True
-    else:
-        # Here we do pos_zero + 1 because in valid_moves
-        # we have the positions from 1 to 9
-        pos_zero = state.index(0) 
-        for d in valid_moves[pos_zero + 1]:
-            temp_state = state.copy()
-            # Same here: d is in [1,9], so we have to subtract 1
-            temp_state[pos_zero], temp_state[d-1]= temp_state[d-1], 0
-            if ([pos_zero, d-1] not in moves \
-                    or [d-1, pos_zero] not in moves) \
-                    and not found: 
-                print("move {0} -> {1}".format(pos_zero+1, d))
-                print("conf: ")
-                show_state(temp_state) 
-                moves.append([pos_zero, d-1])
-                dfs(temp_state)
+def gen_children(state):
+    idx_zero = state.index(0)
+    moves = valid_moves[idx_zero]
+    children = []
+    for move in moves:
+        new_state = state.copy()
+        new_state[move], new_state[idx_zero] = 0, state[move]
+        children.append(new_state)
+    return children
 
-
+def search(state, search_type = "bfs"):
+    if search_type not in ["bfs", "dfs"]: 
+        print("Algoritmo inexistente: ", search_type)
+        return False
+    # TODO: add (state, parent, length)
+    init_state = state.copy()
+    open_states = [state]
+    closed_states = []
+    reps = 0
+    depth_bound = 5
+    while len(open_states) != 0 and (search_type == "bfs" or reps <= 
+            depth_bound):
+        reps += 1
+        cs = open_states[0] # current state
+        open_states.remove(cs)
+        if check_state(cs): 
+            print("\tSolucionado! Caminho... ainda não disponível!")
+            print("State 0: ")
+            show_states_conf(init_state)
+            print("Final state: ")
+            show_states_conf(cs)
+            return True
+        else:
+            print("Lvl ", reps)
+            print("State: ")
+            show_states_conf(cs)
+            
+            cs_children = gen_children(cs)
+            
+            print("Children possible: ")
+            for c in cs_children: show_states_conf(c)
+            
+            closed_states.append(cs)
+            
+            print("Children selected: ")
+            
+            for child in cs_children:
+                if (child not in open_states) and (child not in closed_states):
+                    show_states_conf(child)
+                else:
+                    cs_children.remove(child)
+            if search_type == "dfs":
+                open_states = cs_children + open_states
+            else:
+                open_states.extend(cs_children)
+    print("Solução não encontrada.")
+    return False
+ 
 def main():
-    state = [0, 2, 3, 4, 5, 6, 7, 1, 8]
-    print("Initial state: \n")
-    show_state(state)
-    dfs(state)
-
+    state = [2, 8, 3, 1, 6, 4, 7, 0, 5]
+    #state = [1, 2, 3, 4, 5, 6, 0, 7, 8]
+    print("Lvl 0")
+    show_states_conf(state)
+    print("Com DFS: ")
+    search(state, "dfs")
+    print("##################################")
+    print("Com BFS: ")
+    search(state) # bfs default
+ 
 if __name__ == "__main__":
-    main()
+     main()
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
